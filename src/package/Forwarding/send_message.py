49357077a0cdbcdb086c402814f7c1453425a8c4
ougwen1235 <ougwen1235@gmail.com>
2016-12-13 16:01:01 +0800

    完成文档编写

--- src/package/Forwarding/send_message.py
+++ src/package/Forwarding/send_message.py
@@ -18,6 +18,7 @@ class Application(Frame):
     self.pack()
     self.createWidgets()
 
+  # 构建图形化界面
   def createWidgets(self):
     self.destInput = Entry(self)
     self.destInput.pack(side = LEFT)
@@ -33,6 +34,7 @@ class Application(Frame):
     lock = threading.RLock()
     lock.acquire()
     try:
+      # 只有当数据包和目的节点都不为空才将数据包发出
       if dest == None:
         messagebox.showinfo('Message', 'Please input the destination ip address!')
       elif package.settings.setting.ip_Mapping[dest] == None:
@@ -43,10 +45,12 @@ class Application(Frame):
         destIp = package.settings.setting.ip_Mapping[dest]
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         for dst in package.settings.setting.path_Table:
+          # 选择正确的路径
           if dst[-1] == dest:
             path = dst
             print("PATH %s" % path)
             break
+        # 得到下一个节点
         ip = package.settings.setting.ip_Mapping[path[1]]
         s.sendto(messages_to_json("0",destIp, message).encode('utf-8'), (ip, package.settings.setting.Port))
         s.close()
--- src/package/Listening/Deal_Message/deal_With_Message_Packet.py
+++ src/package/Listening/Deal_Message/deal_With_Message_Packet.py
@@ -1,17 +1,14 @@
 import socket, threading
-# import package.settings.U
-# import package.settings.V
-# import package.settings.W
-# import package.settings.X
-# import package.settings.Y
 import package.settings.setting
 
 def deal_With_Message_Packet(goal_ip, content, new_packet):
     print('DEBUG:----------')
     print('content is %s' %content)
 
+    # 如果目的节点是自身，则将数据包进行输出
+    # 否则，根据迪杰特斯拉路径取得下一个节点，将数据包发出
     if goal_ip == package.settings.setting.ip_Mapping[package.settings.setting.HOST]:
-        print(content)
+        print('Get message: ', content)
     else:
         print('Forwarding')
     flag = False
@@ -19,6 +16,7 @@ def deal_With_Message_Packet(goal_ip, content, new_packet):
     try:
       for path in package.settings.setting.path_Table:
         dest = path[-1]
+        # 取得下一个节点
         if package.settings.setting.ip_Mapping[dest] == goal_ip:
           next_ip = package.settings.setting.ip_Mapping[path[1]]
           break
