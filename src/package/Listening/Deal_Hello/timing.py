3cbc13c4ea78a941cbedde806d0539669f67c4e0
ougwen1235 <ougwen1235@gmail.com>
2016-12-14 20:57:05 +0800

    修改timing

--- src/package/Listening/Deal_Hello/timing.py
+++ src/package/Listening/Deal_Hello/timing.py
@@ -2,16 +2,23 @@ import time
 import package.settings.setting
 
 def timing():
-  for name in package.settings.setting.receiver:
-    package.settings.setting.receiver[name] = False
-  time.sleep(30)
-  router_Self = package.settings.setting.router_Table[package.settings.setting.HOST]
-  router_Names = []
-  if package.settings.setting.receiver != None:
-    for name in package.settings.setting.receiver:
-      if name in package.settings.setting.router_Table.keys() and package.settings.setting.receiver[name] == False:
-        router_Self.pop(name)
-        router_Names.append(name)
+  try:
+    rlock = threading.RLock()
+    rlock.acquire()
 
-  for name in router_Names:
-    package.settings.setting.router_Table.pop(name)
+    for name in package.settings.setting.receiver:
+      package.settings.setting.receiver[name] = False
+    time.sleep(30)
+    router_Names = []
+    if package.settings.setting.receiver != None:
+      for name in package.settings.setting.receiver:
+        if name in package.settings.setting.router_Table.keys() and package.settings.setting.receiver[name] == False:
+          package.settings.setting.router_Table[package.settings.setting.HOST].pop(name)
+          router_Names.append(name)
+
+    for name in router_Names:
+      package.settings.setting.router_Table.pop(name)
+    rlock.release()
+  except socket.error:
+    print("DEBUG::Fail to timing\n")
+
